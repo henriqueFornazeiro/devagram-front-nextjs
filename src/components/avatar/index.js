96f1141a335474496627325ea0aa60d1d927f51a
Henrique Fornazeiro <henrique.fornazeiro@hotmail.com>
2023-05-04 21:07:21 -0300

    avatar component

---
+++ src/components/avatar/index.js
@@ -0,0 +1,21 @@
+import avatarImg from '../../../public/images/avatar.svg'
+
+export default function Avatar({src}){
+
+    const getAvatar = () =>{
+        if(src && src !== "undefined"){
+            return src
+        }
+
+        return avatarImg.src
+
+    }
+
+return(
+    <img
+        src={getAvatar()}
+        alt='Avatar'
+        className='avatar'
+        />
+);
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Button from '../components/button'
-
+import Avatar from '../components/avatar'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
       <h1>Ola mundo</h1>
+      <Avatar/>
       <Button text={"Login"} color={"primary"} disabled={false}/>
     </>
   )
